d9cf26f0539fb9e4943a1ddafbc0129709325385
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-02-17 20:55:16 -0600

    Added a lot to the script.  Now it's a lot cleaner and also has a help.

--- serveo.sh
+++ serveo.sh
@@ -1,35 +1,61 @@
 #!/bin/bash
+SERVEO_HOST=${SERVEO_HOST:-'primo'}
 
-clear;
-echo -e "__________________________________________________                             ";
-echo -e '     __   ___  __        ___  __         ___ ___                               ';
-echo -e '    /__` |__  |__) \  / |__  /  \  |\ | |__   |                                ';
-echo -e '    .__/ |___ |  \  \/  |___ \__/ .| \| |___  |                                ';
-echo -e "__________________________________________________                             ";
-echo -e "                                                                               ";
-echo -e "  parameter [option] : 0 = request random port                                 ";
-echo -e "  parameter [option] [option]: change = Make a random sub domain or choose one.";
-echo -e "  parameter [option] [option] [option]: 4869 = Pick your port to foward"        ;
-echo -e "                                                                               ";
-
-if [[ "$1" = 'https' ]]; then
-    
-    if [[ $3 = 'change' ]]; then
+if [ "$1" == "https" ] || [ "$1" == "http" ]
+then
+    if [[ "$3" != '' ]]; then
+        read -p "serveo: Are you sure you want to use ${3} as your subdomain? " verify
+        if [[ $verify == 'y' || $verify == 'yes' || $verify == 'Y' ]]; then
+            ssh -R $3:443:localhost:$2 $SERVEO_HOST
+        else
+            echo "serveo: Quiting..."
+            sleep 2
+            exit 1
+        fi
+    else
         ssh -R 443:localhost:$2 `echo -n $(date) | md5sum | cut -c1-8`@$SERVEO_HOST
-        elif [[ "$3" -gt "1" ]]; then
-        ssh -R $3:localhost:$2 $SERVEO_HOST
-    else [[ $3 = 0 ]];
-        ssh -R 443:localhost:$2 $SERVEO_HOST
+        echo -e "__________________________________________________                             ";
     fi
-    echo -e "__________________________________________________                             ";
-fi
-
-if [[ "$1" = 'tcp' ]]; then
+elif [[ $1 == 'tcp' ]]
+then
     
-    if [[ "$3" -gt "1" ]]; then
-        ssh -R $3:localhost:$2 $SERVEO_HOST
-    else [[ $3 = 0 ]];
+    if [[ "$3" != '' ]]; then
+        read -p "serveo: Are you sure you want to use ${3} as your tcp port? " verify
+        if [[ $verify == 'y' || $verify == 'yes' || $verify == 'Y' ]]; then
+            ssh -R $3:localhost:$2 $SERVEO_HOST
+        else
+            echo "serveo: Quiting..."
+            sleep 2
+            exit 1
+        fi
+    else
         ssh -R 0:localhost:$2 $SERVEO_HOST
     fi
+else
+    echo "serveo: $(tput setaf 1)error:$(tput sgr0) Usage:"
+    echo "serveo http/https <subdomain> $(tput setaf 1)or$(tput sgr0) serveo tcp <port>"
+    exit 1
     echo -e "__________________________________________________                             ";
 fi
+
+# if [[ "$1" = 'https' ]]; then
+#
+# if [[ $3 = 'change' ]]; then
+# ssh -R 443:localhost:$2 `echo -n $(date) | md5sum | cut -c1-8`@$SERVEO_HOST
+# elif [[ "$3" -gt "1" ]]; then
+# ssh -R $3:localhost:$2 $SERVEO_HOST
+# else [[ $3 = 0 ]];
+# ssh -R 443:localhost:$2 $SERVEO_HOST
+# fi
+# echo -e "__________________________________________________                             ";
+# fi
+#
+# if [[ "$1" = 'tcp' ]]; then
+#
+# if [[ "$3" -gt "1" ]]; then
+# ssh -R $3:localhost:$2 $SERVEO_HOST
+# else [[ $3 = 0 ]];
+# ssh -R 0:localhost:$2 $SERVEO_HOST
+# fi
+# echo -e "__________________________________________________                             ";
+# fi
