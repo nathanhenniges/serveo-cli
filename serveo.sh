a0f351ba92ebea4c8e1d4480b7d729f15a870707
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-02-21 13:54:56 -0600

    Fixed the port require.  Now it will alert if you don't put a port.

--- serveo.sh
+++ serveo.sh
@@ -11,11 +11,22 @@ function port_is_ok {
     local port="$1"
     local -i port_num=$(to_int "${port}" 2>/dev/null)
     
+    if [[ "$port" == '' ]]; then
+        echo 'Port is required to use serveo'
+        echo "Usage: serveo http/https <subdomain> $(tput setaf 1)or$(tput sgr0) serveo tcp <port>"
+        exit 1
+    fi
+    
     if (( $port_num < 1 || $port_num > 65535 )) ; then
         echo "${port} is not a valid port." 1>&2
-        exit 0
+        exit 1
     fi
 }
+
+if [ -z "$1" ]; then
+    echo "Usage: serveo http/https <subdomain> $(tput setaf 1)or$(tput sgr0) serveo tcp <port>"
+    exit 0
+fi
 
 if [ "$1" == "https" ] || [ "$1" == "http" ]
 then
@@ -23,7 +34,7 @@ then
         read -p "serveo: Are you sure you want to use ${3} as your subdomain? " verify
         if [[ $verify == 'y' || $verify == 'yes' || $verify == 'Y' ]]; then
             port_is_ok $2
-            ssh -R $3:443:localhost:$2 $SERVEO_HOST
+            ssh -o LogLevel=QUIET -R $3:443:localhost:$2 $SERVEO_HOST
         else
             echo "serveo: Quiting..."
             sleep 2
@@ -31,8 +42,7 @@ then
         fi
     else
         port_is_ok $2
-        ssh -R 443:localhost:$2 `echo -n $(date) | md5sum | cut -c1-8`@$SERVEO_HOST
-        echo -e "__________________________________________________                             ";
+        ssh -o LogLevel=QUIET -R 443:localhost:$2 `echo -n $(date) | md5sum | cut -c1-8`@$SERVEO_HOST
     fi
 elif [[ $1 == 'tcp' ]]
 then
@@ -41,7 +51,7 @@ then
         read -p "serveo: Are you sure you want to use ${3} as your tcp port? " verify
         if [[ $verify == 'y' || $verify == 'yes' || $verify == 'Y' ]]; then
             port_is_ok $3
-            ssh -R $3:localhost:$2 $SERVEO_HOST
+            ssh -o LogLevel=QUIET -R $3:localhost:$2 $SERVEO_HOST
         else
             echo "serveo: Quiting..."
             sleep 2
@@ -49,11 +59,10 @@ then
         fi
     else
         port_is_ok $2
-        ssh -R 0:localhost:$2 $SERVEO_HOST
+        ssh -o LogLevel=QUIET -R 0:localhost:$2 $SERVEO_HOST
     fi
 else
-    echo "serveo: $(tput setaf 1)error:$(tput sgr0) Usage:"
-    echo "serveo http/https <subdomain> $(tput setaf 1)or$(tput sgr0) serveo tcp <port>"
-    exit 1
+    echo "Usage: serveo http/https <subdomain> $(tput setaf 1)or$(tput sgr0) serveo tcp <port>"
     echo -e "__________________________________________________                             ";
+    exit 1
 fi
